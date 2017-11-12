c80645498adba93354f772071d07a0d2c9e9c1fb
DaniloBojovic <danilobojovic0@gmail.com>
2017-11-12 10:54:09 +0100

    Importig project

---
+++ FormTreeView.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class FormTreeView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolderName = new System.Windows.Forms.TextBox();
+            this.btnBrowseFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnCreateXml = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selected path:";
+            // 
+            // txtFolderName
+            // 
+            this.txtFolderName.BackColor = System.Drawing.SystemColors.Window;
+            this.txtFolderName.Location = new System.Drawing.Point(85, 34);
+            this.txtFolderName.Name = "txtFolderName";
+            this.txtFolderName.Size = new System.Drawing.Size(182, 20);
+            this.txtFolderName.TabIndex = 1;
+            // 
+            // btnBrowseFolder
+            // 
+            this.btnBrowseFolder.Location = new System.Drawing.Point(273, 34);
+            this.btnBrowseFolder.Name = "btnBrowseFolder";
+            this.btnBrowseFolder.Size = new System.Drawing.Size(75, 20);
+            this.btnBrowseFolder.TabIndex = 2;
+            this.btnBrowseFolder.Text = "Browse";
+            this.btnBrowseFolder.UseVisualStyleBackColor = true;
+            this.btnBrowseFolder.Click += new System.EventHandler(this.btnBrowseFolder_Click);
+            // 
+            // btnCreateXml
+            // 
+            this.btnCreateXml.Location = new System.Drawing.Point(12, 99);
+            this.btnCreateXml.Name = "btnCreateXml";
+            this.btnCreateXml.Size = new System.Drawing.Size(152, 43);
+            this.btnCreateXml.TabIndex = 3;
+            this.btnCreateXml.Text = "Create XML ";
+            this.btnCreateXml.UseVisualStyleBackColor = true;
+            this.btnCreateXml.Click += new System.EventHandler(this.btnCreateXml_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 161);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(336, 189);
+            this.treeView1.TabIndex = 5;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(393, 161);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 13);
+            this.lblInfo.TabIndex = 6;
+            // 
+            // FormTreeView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 416);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnCreateXml);
+            this.Controls.Add(this.btnBrowseFolder);
+            this.Controls.Add(this.txtFolderName);
+            this.Controls.Add(this.label1);
+            this.Name = "FormTreeView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tree View Form";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolderName;
+        private System.Windows.Forms.Button btnBrowseFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnCreateXml;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
---
+++ FormTreeView.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication4
+{
+    public partial class FormTreeView : Form
+    {
+        string directoryPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\TreeView.xml";
+        XDocument document = null;
+        public FormTreeView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            txtFolderName.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnCreateXml_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(txtFolderName.Text))
+            {
+                string header = "<?xml version=\"1.0\" encoding=\"utf-8\" ?><Directory></Directory> ";
+                document = XDocument.Parse(header);
+                XElement root = document.Root;
+                CreateXmlRecursion(txtFolderName.Text, root);                
+                document.Save(directoryPath);
+                MessageBox.Show("Xml file successfully created.");
+                if (document != null)
+                {
+                    treeView1.Nodes.Clear();
+                    TreeNode treeNode = new TreeNode(document.Root.FirstNode.ToString());
+                    AddNode(document.Root, treeNode);
+                    treeView1.Nodes.Add(treeNode);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Browse directory.");
+            }
+        }
+
+        private float CreateXmlRecursion(string path, XElement element)
+        {
+            element.SetValue(path);
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            float folderSize = 0;
+            int numOfFiles = 0;
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                try
+                {
+                    float fileSize = file.Length;
+                    folderSize += fileSize;
+                    element.Add(new XElement("File", new object[] {
+                        new XAttribute("FileName",file.Name),
+                        new XAttribute("FileSize",fileSize+"(bytes)"),
+                        new XAttribute("FileCreationTime",file.CreationTime),
+                        new XAttribute("FileLastAccessTime",file.LastAccessTime),
+                        new XAttribute("FileLastModifiedTime",file.LastWriteTime)}));
+                    numOfFiles++;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            foreach (string directory in Directory.GetDirectories(path))
+            {
+                DirectoryInfo info = new DirectoryInfo(directory.ToString());
+                XElement subDirectory = new XElement("Directory");
+                element.Add(subDirectory);
+                float sudDirSize = CreateXmlRecursion(directory, subDirectory);
+                folderSize += sudDirSize;
+            }
+            element.Add(new XAttribute[]
+            {
+                new XAttribute("FolderName",directoryInfo.Name),
+                new XAttribute("FolderSize",folderSize+"(bytes)"),
+                new XAttribute("NumberOfFiles",numOfFiles)
+            });
+            return folderSize;
+        }
+
+        private void AddNode(XElement xElement, TreeNode treeNode)
+        {
+            treeView1.Nodes.Clear();
+            foreach (var att in xElement.Attributes())
+            {
+                treeNode.Text = treeNode.Text;
+                treeNode.Tag = xElement;
+            }
+            foreach (XElement childElement in xElement.Elements())
+            {
+                TreeNode tNode = treeNode.Nodes.Add(childElement.FirstAttribute.Value);
+                AddNode(childElement, tNode);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            var xElement = treeView1.SelectedNode.Tag as XElement;
+
+            if (xElement != null)
+            {
+                lblInfo.Text = "";
+                foreach (var a in xElement.Attributes())
+                {
+                    lblInfo.Text += a.Name.ToString() + ": " + a.Value + "\r\n" + "\r\n";
+                }                
+            }
+            else
+            {
+                lblInfo.Text = "";
+            }
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
